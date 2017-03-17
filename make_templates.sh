a79f68c13019fa67aae821e0fc1d1c0bbeffadfc
Stefan Grosshauser <Stefan.Grosshauser@uni-bayreuth.de>
2017-03-17 23:09:59 +0100

    add labels and scan cellsize

--- make_templates.sh
+++ make_templates.sh
@@ -3,13 +3,29 @@
 template_file=miura_ori_template.ps
 
 template_name=${template_file%.ps}
+folder="scan"
+
+mkdir -p $folder
+
 echo "processing $template_name..."
 
 
-for val in 10 15 20 45 50 60 70
+for cellx in 2 3 4 5
 do
-    echo "for $val degree"
-    sed "s/winkel.*def/winkel $val def/" "$template_file" > "${template_name}_$val.ps"
 
+    for celly in 2 3 4 5
+    do
+
+	for alpha in 10 15 20 45 50 60 70
+	do
+	    echo "for $alpha degree"
+	    newfile="$folder/${template_name}_${alpha}_${cellx}_${celly}.ps"
+	    sed "s|/winkel .*def|/winkel $alpha def|" "$template_file" > "$newfile"
+	    sed -i "s|/xabstandcm .*def|/xabstandcm $cellx def|" "$newfile"
+	    sed -i "s|/yabstandcm .*def|/yabstandcm $celly def|" "$newfile"
+
+	done
+
+    done
 done
 	   
